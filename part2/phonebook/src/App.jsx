7109eebff6ed11e1606ff3224f256bc18c23dfb4
Lucia Sotomayor <cia811114@gmail/com>
2025-03-21 07:21:40 -0700

    phonebook

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('Arturo')
+
+  const handlerClickAdd = (event) => {
+    event.preventDefault()
+    
+    if(newName) {
+      setPersons(persons.concat({name: newName}))
+      setNewName('')
+    }
+ }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={(event) => setNewName(event.target.value)}/>
+        </div>
+        <div>
+          <button type="submit" onClick={handlerClickAdd}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {
+        persons.map( item => <div key={item.name}>{item.name}</div>)
+      }
+    </div>
+  )
+}
+
+export default App

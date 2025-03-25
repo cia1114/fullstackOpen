08ce316b3f32d21e9fcc97057d25f4b8734fa8f8
Lucia Sotomayor <cia811114@gmail/com>
2025-03-25 10:07:59 -0700

    exercise 2.11 phonebook step 6 - getting data

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -2,10 +2,11 @@
 const Person = ({id, name, number}) => <div key={id}>{name} {number} </div>
 
 export const Persons = ({persons}) => {
+  
   return (
     <div>
         {
-            persons.map( item => <Person id={item.id} name={item.name} number={item.number} />)
+            persons.map( item => <Person key={item.id} id={item.id} name={item.name} number={item.number} />)
         }
     </div>
   )
